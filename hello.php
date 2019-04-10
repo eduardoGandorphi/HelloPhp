630a2cc0f125f80e1bae3c7667e1f156ca45e8fd
Eduardo Gandorphi <eduardogandorphi@outlook.com>
2019-04-09 22:08:02 -0300

    if else

--- hello.php
+++ hello.php
@@ -5,11 +5,46 @@ echo "to php<br>";
 
 $name = 'Hello World';
 $number = 10;
-echo "$name number $number";
+echo "$name number $number<br>";
 
 //THIS IS A COMMENT
 /*also
 this is a
 comment*/
 
+
+echo "double quotes<br>";
+echo 'simple quotes<br>';
+
+
+$Word = 'world';
+
+echo "test $Word<br>";
+echo 'test $Word<br>';
+echo 'test '.$Word.'!<br>';
+
+echo '<a href="https://www.google.com">GOOGLE</a><br>';
+
+echo 'quero um como d\'agua<br>';
+
+echo  5 + 2 .'<br>';
+echo  5 - 2 .'<br>';
+echo  5 / 2 .'<br>';
+echo  5 * 2 .'<br>';
+
+$num = 5 + 2;
+echo  $num . '<br>';
+$num++;
+echo  $num .'<br>';
+$num--;
+echo  $num .'<br>';
+
+
+$inputed = 'test';
+if ($inputed == 'test'){
+	echo 'yes';
+} else {
+	echo 'no';
+}
+echo '<br>';
 ?>
