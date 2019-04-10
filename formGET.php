1cbfacae5cf6925a3d9c2d439e9e6b3e5545c2db
Eduardo Gandorphi <eduardogandorphi@outlook.com>
2019-04-09 22:48:22 -0300

    forms

---
+++ formGET.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FORM</title>
+</head>
+<body>
+	<form action="hello.php" method="get">
+		<b>Nome:</b>		
+		<input type="text" name="nomeParam">
+		<br>
+		<b>Idade:</b>		
+		<input type="text" name="anoParam">	
+		<br>
+		<input type="submit" value="Get">
+	</form>
+</body>
+</html>
--- hello.php
+++ hello.php
@@ -2,10 +2,12 @@
 
 echo "Welcome <br>";
 echo "to php<br>";
+echo '<br><br>';
 
 $name = 'Hello World';
 $number = 10;
 echo "$name number $number<br>";
+echo '<br><br>';
 
 //THIS IS A COMMENT
 /*also
@@ -16,16 +18,19 @@ comment*/
 echo "double quotes<br>";
 echo 'simple quotes<br>';
 
-
 $Word = 'world';
 
 echo "test $Word<br>";
 echo 'test $Word<br>';
 echo 'test '.$Word.'!<br>';
+echo '<br><br>';
 
 echo '<a href="https://www.google.com">GOOGLE</a><br>';
+echo '<br><br>';
 
 echo 'quero um como d\'agua<br>';
+echo '<br><br>';
+
 
 echo  5 + 2 .'<br>';
 echo  5 - 2 .'<br>';
@@ -38,6 +43,7 @@ $num++;
 echo  $num .'<br>';
 $num--;
 echo  $num .'<br>';
+echo '<br><br>';
 
 
 $inputed = 'test2';
@@ -50,7 +56,8 @@ if ($inputed == 'test'){
 }else {
 	echo 'no';
 }
-echo '<br>';
+echo '<br><br>';
+
 
 
 // == > >= < <= !=
@@ -64,17 +71,20 @@ if($nome == 'edu' and $sobre == 'gand'){
 }else if($nome == 'edu' and $sobre == 'cinzento'){
 	echo 'edu cinzento';
 }
-echo '<br>';
+echo '<br><br>';
+
 
 
 //$myArray $arrayName = array('' => , );
 $myArray = array('joao','maria','jose');
 echo $myArray[1];
-echo '<br>';
+echo '<br><br>';
+
 
 $idades = array('joao' => 10,'maria'=>16,'jose'=>20);
 echo $idades['jose'];
-echo '<br>';
+echo '<br><br>';
+
 
 print_r($idades);
 echo '<br><br>';
@@ -91,6 +101,11 @@ foreach ($myArray as $i) {
 	echo 'looping, @'.$i.'<br>';
 }
 echo '<br><br>';
+
+$nomeParam = $_GET['nomeParam'];
+$anoParam = $_GET['anoParam'];
+echo $nomeParam.' '.$anoParam;
+
 
 
 echo '<br><br>';
